d00a04854042167e8da2245707bf0b857f7ab046
Brandon Hays <brandon.hays@gmail.com>
2015-02-26 02:59:42 -0600

    download image implemented

--- app/routes/images/show.js
+++ app/routes/images/show.js
@@ -4,5 +4,14 @@ import imageData from 'ember-cli-sample-image-data';
 export default Ember.Route.extend({
   model: function(params) {
     return imageData(params.id);
+  },
+  actions: {
+    downloadImage: function() {
+      var link = document.createElement("a");
+      var uri = Ember.$("img.pixelated-image").attr("src");
+      link.download = "pixelfy.png";
+      link.href = uri;
+      link.click();
+    }
   }
 });
